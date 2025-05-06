4eed7432cfcd51b0df38f6a538805c79c6e2fb90
daniel divney <danieldivney@daniels-MacBook-Pro-2.local>
2025-05-06 15:37:58 -0600

    Updates to view

---
+++ pages/Clickfunnels.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+    <div class="main">
+    <div class="grid">
+<div class="left">
+    <h4>clickfunnels</h4>
+    <p class="tm">Increased stick rate by 3% with end to end design, from problem to high fidelity solution using proven design thinking methodologies.</p>
+</div>
+<div class="right"></div>
+    </div>
+</div>
+</template>
+
+
+
+<style>
+
+.main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: auto;
+    width: 100vw;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(16, 1fr);
+  column-gap: 32px;
+  width: 1280px;
+}
+
+
+.left {
+  display: flex;
+  flex-direction: column;
+  grid-column: span 6;
+  border: solid 1px black;
+  background-color: #21242D;
+  color: white!important;
+  padding: 24px;
+  gap: 12px;
+  border-radius: 24px;
+}
+.right {
+  grid-column: span 10;
+  border: solid 1px black;
+  border-radius: 24px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,24 @@
 </script>
 
 <template>
-<h1>hello world</h1>
+<h1>header 1</h1>
+<h2>header 2</h2>
+<h3>header 3</h3>
+<h4>header 4</h4>
+<h5>header 5</h5>
+
+<p class="ts"> this is some text</p>
+<p class="tm"> this is some text</p>
+<p class="tl"> this is some text</p>
+
+<br>
+
+<h4>this is an H4</h4>
+<p class="ts">this is some text underneath the header</p>
 
 </template>
 
 
-<style lang="scss" scoped>
+<style>
 
 </style>
