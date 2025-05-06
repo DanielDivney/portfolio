1dda8f6856d15805ba36fe7efbd7708fb64686dd
daniel divney <danieldivney@daniels-MacBook-Pro-2.local>
2025-05-06 14:01:39 -0600

    Added CSS and pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/scripts']
+  modules: ['@nuxt/image', '@nuxt/scripts'],
+  css: ['@/css/main.css']
 })
